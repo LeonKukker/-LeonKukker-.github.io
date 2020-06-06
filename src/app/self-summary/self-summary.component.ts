4828e3b8c95655096d705faf47a200b45472c81c
LeonKukker <30826099+LeonKukker@users.noreply.github.com>
2020-06-06 16:40:07 +0300

    build commit

---
+++ src/app/self-summary/self-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-self-summary',
+  templateUrl: './self-summary.component.html',
+  styleUrls: ['./self-summary.component.sass']
+})
+export class SelfSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
